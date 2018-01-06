8d84078eadeb3b7950062e327df05459730928c7
DilyanaToneva <32540861+DilyanaToneva@users.noreply.github.com>
2018-01-06 17:28:36 +0200

    commit message

---
+++ task2/test2.js
@@ -0,0 +1,70 @@
+"use strict"
+
+// задача 1 (4т.)
+// Сортирайте следния списък от числа във възходящ ред:
+var arr = [2, 5, 8, 4, 1, 12];
+arr.sort(function(a,b){
+    return a-b;
+});
+console.log("arr",arr);
+
+// задача 2 (4т.)
+// Напишете функция, която да изважда всички думи, с дължина над 4 символа от следния текст:
+var text = "The quick brown fox jumps over the lazy dog";
+
+function extractWorld(text,desiredLenght){
+    var worldsArr = text.split(" ");
+    var extracted;
+    extracted = worldsArr.filter(function(word){
+        return world.length > 4;
+    });
+}
+console.log("extracted",extracted);
+return extracted;
+
+// задача 3 (5т.)
+// Напишете код, който на всяка секунда закача към `#container` елемента следния html елемент: `<p>repetition is fun</p>`
+function moveElement()
+{
+    var containerElement = document.querySelector("container");
+    var childElement =  document.createElement("p");
+    childElement.innerHTML = "repetition is fun";
+   
+}
+console.log("conteiner",conteiner);
+
+// задача 4 (6т.)
+// Направете така, че 2 секунди след зареждането на дадена страница, всички картинки в нея да се завъртят по вертикалната си ос (по Y)
+
+// ...
+
+// задача 5 (6т.)
+// Напишете функция, която да сортира следния списък от обекти по полето `price`:
+var items = [
+    {
+        id: 1,
+        title: "Item 1",
+        price: 4.3
+    },
+    {
+        id: 2,
+        title: "Item 2",
+        price: 2.0
+    },
+    {
+        id: 3,
+        title: "Item 3",
+        price: 6.5
+    },
+    {
+        id: 4,
+        title: "Item 4",
+        price: 1.5
+    }
+];
+function sortItemByPrice(items){
+    items.sort(price);
+    
+    return result;
+}
+console.log(sortItemByPrice);
